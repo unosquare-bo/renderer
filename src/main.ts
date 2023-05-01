6640a0bbe83d4b8ba69e61486e2e639adc19ab08
Diego Landa <diegolanda@msn.com>
2023-05-01 16:54:39 -0400

    update

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+if (
+  process.env.LD_LIBRARY_PATH == null ||
+  !process.env.LD_LIBRARY_PATH.includes(
+    `${process.env.PWD}/node_modules/canvas/build/Release:`,
+  )
+) {
+  process.env.LD_LIBRARY_PATH = `${
+    process.env.PWD
+  }/node_modules/canvas/build/Release:${process.env.LD_LIBRARY_PATH || ''}`;
+}
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
