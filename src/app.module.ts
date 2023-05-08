dab67e29691a4a6f52c6eb8a4390e69038fe08fa
Ronald Escobar <ronald.escobar@unosquare.com>
2023-05-08 09:24:02 -0400

    Added nestJS axios package for http. Added content to sirvCdnService, including baseUrl and a function to get token providing client ID and secret. Addd interface for token response.

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { RendererController } from './common/controllers/renderer/renderer.controller';
 import { RendererService } from './common/services/renderer/renderer.service';
 import { ConfigModule } from '@nestjs/config';
+import { HttpModule } from "@nestjs/axios";
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [ConfigModule.forRoot(), HttpModule],
   controllers: [AppController, RendererController],
   providers: [AppService, RendererService],
 })
