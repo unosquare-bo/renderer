b88efae55a3165909e15ab5bb0fe4b518f27466b
Ronald Escobar <ronald.escobar@unosquare.com>
2023-05-12 12:35:35 -0400

    Extracted logic to add interceptors to a separate util function called addInterceptors, which receives httpInstance, Url and token utils to get, set and refresh token based on each service. Added tokenUtils interface. Added logic in both SirvCDN and Slackbot services to build token utils and use addInterceptors for adding the interceptors.

---
+++ src/common/utils/add-interceptor.util.ts
@@ -0,0 +1,31 @@
+import { HttpService } from "@nestjs/axios";
+import TokenUtils from "../types/TokenUtils";
+
+export default function addInterceptors(httpService: HttpService, urlToValidate: string, tokenUtils: TokenUtils): HttpService {
+  httpService.axiosRef.interceptors.request.use(config => {
+    if (config.url.includes(urlToValidate)) {
+      config.headers.Authorization = `Bearer ${tokenUtils.getToken()}`;
+    }
+    return config;
+  });
+  httpService.axiosRef.interceptors.response.use(
+    response => {
+      return response;
+    },
+    async error => {
+      const originalConfig = error.config;
+      if (
+        originalConfig.url.includes(urlToValidate) &&
+        error.response.status === 401 &&
+        !originalConfig._retry
+      ) {
+        originalConfig._retry = true;
+        const { token } = await tokenUtils.refreshToken();
+        tokenUtils.setToken(token);
+        originalConfig.headers.Authorization = `Bearer ${token}`;
+        return httpService.axiosRef(originalConfig);
+      }
+      return Promise.reject(error);
+    });
+  return httpService;
+}
