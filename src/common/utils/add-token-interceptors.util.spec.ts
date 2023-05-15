d9ae60e250574903ac097f23ef63b7cd5269d988
Ronald Escobar <ronald.escobar@unosquare.com>
2023-05-15 12:24:57 -0400

    Refactored.

--- src/common/utils/add-token-interceptors.util.spec.ts
+++ src/common/utils/add-token-interceptors.util.spec.ts
@@ -4,6 +4,7 @@ import addTokenInterceptors from './add-token-interceptors.util';
 describe('addTokenInterceptors', () => {
   const tokenExpected = 'test';
   const fakeUrl = 'test';
+  let mockRejectionData = {};
 
   let httpService: HttpService;
   let testToken = '';
@@ -20,6 +21,10 @@ describe('addTokenInterceptors', () => {
   beforeEach(async () => {
     httpService = new HttpService();
     testToken = '';
+    mockRejectionData = {
+      config: { url: fakeUrl, _retry: false, headers: {} },
+      response: { status: 401 },
+    };
     jest.spyOn(httpService.axiosRef, 'post').mockImplementation((config) => {
       return Promise.resolve(config)
     });
@@ -27,20 +32,17 @@ describe('addTokenInterceptors', () => {
 
   it('should have bearer token in header after rejection', async () => {
     httpService = addTokenInterceptors(httpService, fakeUrl, tokenUtils);
-    const rejected = await (httpService.axiosRef.interceptors.response as any).handlers[0].rejected({
-      config: { url: fakeUrl, _retry: false, headers: {} },
-      response: { status: 401 },
-    });
+    const { response } = httpService.axiosRef.interceptors;
+    const rejected = await (response as any).handlers[0].rejected(mockRejectionData);
     expect(rejected.headers.Authorization).toBe(`Bearer ${tokenExpected}`);
   });
 
   it('should have bearer token in header after rejection and new request afterwards', async () => {
     httpService = addTokenInterceptors(httpService, fakeUrl, tokenUtils);
-    await (httpService.axiosRef.interceptors.response as any).handlers[0].rejected({
-      config: { url: fakeUrl, _retry: false, headers: {} },
-      response: { status: 401 },
-    });
-    const fulfilled = await (httpService.axiosRef.interceptors.request as any).handlers[0].fulfilled({ url: 'test', headers: {} })
+    const { request, response } = httpService.axiosRef.interceptors;
+    await (response as any).handlers[0].rejected(mockRejectionData);
+    const mockFulfilledData = { url: fakeUrl, headers: {} };
+    const fulfilled = await (request as any).handlers[0].fulfilled(mockFulfilledData);
     expect(fulfilled.headers.Authorization).toBe(`Bearer ${tokenExpected}`);
   });
 });
