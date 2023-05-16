8b9e07e27523ff06288264025c1f5ce484208370
Ronald Escobar <ronald.escobar@unosquare.com>
2023-05-16 08:54:14 -0400

    Finished RendererParameters class parameters with required for topic.

--- src/common/types/RendererParameters.ts
+++ src/common/types/RendererParameters.ts
@@ -3,4 +3,12 @@ import { IsEmail, IsNotEmpty } from 'class-validator';
 export class RendererParameters {
   @IsNotEmpty()
   name: string
+
+  title: string;
+  subtitle: string;
+  uid: string;
+  date: string;
+
+  @IsNotEmpty()
+  topic: string;
 }
