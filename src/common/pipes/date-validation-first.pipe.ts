9791d8d8e296f474d44c4b7ff2401a12ef49eaac
Ronald Escobar <ronald.escobar@unosquare.com>
2023-05-17 18:15:29 -0400

    Added types and removed metadata from date pipe Added test file for DateValidationFirstPipe including fake data, test for invalid date with exception and test for valid date.

--- src/common/pipes/date-validation-first.pipe.ts
+++ src/common/pipes/date-validation-first.pipe.ts
@@ -1,9 +1,10 @@
-import { ArgumentMetadata, BadRequestException, Injectable, PipeTransform } from "@nestjs/common";
+import { BadRequestException, Injectable, PipeTransform } from "@nestjs/common";
 import { isDateString } from "class-validator";
+import { RendererParameters } from "../types/RendererParameters";
 
 @Injectable()
-export class DateValidationFirstPipe implements PipeTransform {
-  transform(value: any, metadata: ArgumentMetadata) {
+export class DateValidationFirstPipe implements PipeTransform<RendererParameters, RendererParameters> {
+  transform(value: RendererParameters): RendererParameters {
     if (!isDateString(value.date)) {
       throw new BadRequestException('Date should be in ISO format');
     }
