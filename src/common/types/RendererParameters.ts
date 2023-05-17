b46c4e7fa1a3e1663a5028375565a7ae8039c4d6
Ronald Escobar <ronald.escobar@unosquare.com>
2023-05-17 07:29:16 -0400

    Added timezone UTC to keep date.

--- src/common/types/RendererParameters.ts
+++ src/common/types/RendererParameters.ts
@@ -17,10 +17,12 @@ export class RendererParameters {
   @Transform(({ value }) => new Date(value).toLocaleDateString('en-US', {
     year: 'numeric',
     month: 'long',
-    day: 'numeric'
+    day: 'numeric',
+    timeZone: 'UTC'
   }))
   date: string;
 
   @IsNotEmpty()
   topic: string;
 }
+/* todo update readme, sirvcdn para validar uid, sirvcdn para validar topic, tests, UTC */
